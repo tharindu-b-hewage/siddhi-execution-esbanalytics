f234de2d925202a818da79887d4f605799943cea
LorddBaelish <bandara14@live.com>
2018-08-17 18:23:05 +0530

    Modify test message

--- component/src/test/java/org/wso2/extension/siddhi/execution/esbanalytics/util/ESBAnalyticsDecompressConstants.java
+++ component/src/test/java/org/wso2/extension/siddhi/execution/esbanalytics/util/ESBAnalyticsDecompressConstants.java
@@ -8,24 +8,6 @@ public class ESBAnalyticsDecompressConstants {
         public static final boolean META_COMPRESSED = true;
         public static final int META_TENANT_ID = -1234;
         public static final String MESSAGE_ID = "urn_uuid_0542abda-fa1e-4e95-9543-b63c690f465f199208030396291";
-//        public static final String FLOW_DATA =
-//                "H4sIAAAAAAAAAI2WT4jcVBzHN8lSmlJFRYsUkSoiHszs+5vk5aTorl1odXEvgofh7eTtTjCbjJlkyyKCiGfx" +
-//                        "IJ48FE9FRLa1tHry2vaiXjxVqLB762nbQy8WXzZ5M5PkzerA8Jskv/e+n9+f95sYi5axLcZjviVW4v" +
-//                        "TS6r5lFFnSL4oo7ANKEN8IubPJoXCIYNRhlGBnw8UDl4FN4tJNyBgCPsAAMxexq5Yx4rtxysMH9wxj" +
-//                        "wTKG6fihZUDQgwD0vB6E0kPsiCQvnz8nv09bEuDNtduWcV7wOB8OeCbkpbnQvLH6BggaDvaVX3/7+5" +
-//                        "8P7Mt3jsyjUwvyYxkfwr+sJ0pBFzLku+CXcqPp1VRw9dz7YpwW2eDAMpaGk52XBjwXW2kWifHSJ/Xv" +
-//                        "3U+XMjEW2c6BabapWKC2sX9oAD1UQEgD5EAKPR/7gNxQROvi40IkJY3ctr/67vryH+ZiWw2CYPJ0nh7" +
-//                        "WJYC6LiTQ9W4quYsijHieHlrGWpaORJbv1ney4Hw6HkU5v2+e6MjDYK63fbfCOazMmZqG6KKnBBDq+/" +
-//                        "BqTfOShkJqhv2k2N4Qh+bJDgjqgswsUBC1WahZqC4zxPcwhmBvivI2z/lFPpK7qySZpzoAOOi6Kb0/b" +
-//                        "x2Z/ZO1rqvtACjPi0t8dyYJ65eifDCcqJ7uqJKg6WJ/d2u2Bx4btaKnUUSUyDPL0E9TvQvp1kTsyY4Y" +
-//                        "DWae21cqpe8rY9ZCvkYIAyKrS8nPx1S3yKLeDs96Q9VrT3Xk3W6J26sUTZ3w12sopoFiEFMXed5Mtt/" +
-//                        "icTwJ/5mOvhfMOiipu7ePzDWVaQi0xUUEedhD7Lo6b8tJOEqjRM7CdzKehO/xj5Z/N5+duVqTA276yP" +
-//                        "6xkrvXltONN4dBz5PV9f5PxmXHSo58W47gfrRvnmnHjbz5eW+utfcruMeVOasQdQMPUgp89h9NEaYDa" +
-//                        "fqhyHkUPzCf76D5XbTmGsXy2Z1Gl0LtTPQZIYgy77gpNOJ5VEarmM52mFiXqbVI0XxRmUUFpRuNSP7l" +
-//                        "Ao9CdGNOn77QBsCg2adzxLSzD0AXA0aAGgkXpNiQJ4mIo2RrRQjZoi+2b5V92nKyP6/EvqzMaaWpn3u" +
-//                        "YMQoxZHvHpJ0P8oLH/U1xYJ7rBKyZ/VN/BfFVqwN0E9FBAGHiYwRmW6B6fVnhZV/tTvL+cgcDB3pXJf" +
-//                        "1NZU4oAt2odCBB0MOM4r059X6lo0ua9f66oTaJVzcDHfkORhmigN2cFnxd5HksZCjlqZYFf7V9qyx4y" +
-//                        "0mJflsZ9T+HdLMQMgiI62N6fRqhfGUapUk4CfK1TpBu0PJRWpcbfZ0U8X3dySZSFEPv2r95HRx/3QoAAA==";
         public static final String FLOW_DATA = "H4sIAAAAAAAAAO2ZTW/bNhjHKzstkqwoshx2HFRfChTWC2W9WajauomzZMuL" +
         "Zztdix0EWqIdtTKpSrKTLNhln2IfoPe1Q4d236DYoduA7lJsQA/bbadupwEDRilyauVtzbC2KWpfKInk85APn/9PFM2M5ZkuCkP" +
         "YQXMeWV/4Nc/0Amz1eq5jiS1dlaSWxImgLHKyUrK5ckmBXAuIWrlsA4BERwZAL0s60BVd1+/kGR9uegQ6fzxlmNPf55hbDC3pNZ" +
@@ -49,5 +31,10 @@ public class ESBAnalyticsDecompressConstants {
         "eN2lobDXIoRIpXYD3seK4lAZ0XNkGSjpLAfLDX/C3uUVlmhS6twMqSClZFT5lqaRKWrq1BuO7oDYuRedaFJMc32UacXxChS90GR" +
         "lART0r59/ik1fJByOgsiNXOQknLo8XZx6nhzSB1xaMShEYeOA4e0fb7pZEDVD0Tl/gFHOmeyINKyRzqPMzui8eNNIm1EohGJRiQ" +
         "6BiTCPe/3vVsiGdD5aaL+4B/czklVFSUAAA==";
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(TestData.FLOW_DATA);
     }
 }
